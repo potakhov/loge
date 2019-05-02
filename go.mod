d4194bf9c444293adca13bc1fc7e76e436124953
Alex Jeannopoulos <alexj@paltalk.com>
2019-05-02 15:35:54 -0400

    added module definition, and a wrapped transport - allowing for easying adding of new transport, also added test example

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/potakhov/loge
+
+go 1.12
+
+require github.com/potakhov/cache v0.0.1
---
+++ transport.go
@@ -0,0 +1,91 @@
+package loge
+
+import (
+    "sync"
+)
+
+type TransactionHandler interface {
+    WriteOutTransaction(tr *Transaction)
+    FlushTransactions()
+}
+
+type WrappedTransport struct {
+    buffer      TransactionList
+    signal      chan struct{}
+    done        chan struct{}
+    trans       []uint64
+    transLocker sync.Mutex
+    wg          sync.WaitGroup
+    terminated  bool
+
+    handler TransactionHandler
+}
+
+func WrapTransport(buffer TransactionList, handler TransactionHandler) *WrappedTransport {
+    ft := &WrappedTransport{
+        buffer:  buffer,
+        handler: handler,
+        done:    make(chan struct{}),
+        signal:  make(chan struct{}, 1),
+        trans:   make([]uint64, 0),
+    }
+
+    go ft.loop()
+    return ft
+}
+
+func (ft *WrappedTransport) loop() {
+    ft.wg.Add(1)
+    defer ft.wg.Done()
+
+    for {
+        select {
+        case <-ft.done:
+            ft.flushAll()
+            return
+        case <-ft.signal:
+            ft.flushAll()
+        }
+    }
+}
+
+func (ft *WrappedTransport) NewTransaction(id uint64) {
+    ft.transLocker.Lock()
+    ft.trans = append(ft.trans, id)
+    ft.transLocker.Unlock()
+
+    select {
+    case ft.signal <- struct{}{}:
+    default:
+    }
+}
+
+func (ft *WrappedTransport) Stop() {
+    close(ft.done)
+    ft.wg.Wait()
+}
+
+func (ft *WrappedTransport) flushAll() {
+    if ft.terminated {
+        return
+    }
+
+    ft.transLocker.Lock()
+    if len(ft.trans) == 0 {
+        ft.transLocker.Unlock()
+        return
+    }
+
+    ids := ft.trans
+    ft.trans = make([]uint64, 0)
+    ft.transLocker.Unlock()
+
+    for _, id := range ids {
+        tr, ok := ft.buffer.Get(id, true)
+        if ok {
+            ft.handler.WriteOutTransaction(tr)
+        }
+    }
+
+    ft.handler.FlushTransactions()
+}
